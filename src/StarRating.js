9242c7889707d85622d7943a9d45323bb1c6ff1e
andronicos23 <andronicos.nicolaides@gmail.com>
2024-11-20 17:52:58 +0200

    star

--- src/StarRating.js
+++ src/StarRating.js
@@ -14,8 +14,11 @@ export default function StarRating({
   maxRating = 5,
   color = "#fcc419",
   size = 48,
+  className = "",
+  messages = [],
+  defaultRating = 0,
 }) {
-  const [rating, setRating] = useState(0);
+  const [rating, setRating] = useState(defaultRating);
   const [tempRating, setTempRating] = useState(0);
 
   function handleRating(rating) {
@@ -33,7 +36,7 @@ export default function StarRating({
   };
 
   return (
-    <div style={containerStyle}>
+    <div style={containerStyle} className={className}>
       <div style={starContainerStyle}>
         {Array.from({ length: maxRating }, (_, i) => (
           <Star
@@ -47,7 +50,11 @@ export default function StarRating({
           />
         ))}
       </div>
-      <p style={textStyle}>{tempRating || rating || ""}</p>
+      <p style={textStyle}>
+        {messages.length === maxRating
+          ? messages[tempRating ? tempRating - 1 : rating - 1]
+          : tempRating || rating || ""}
+      </p>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -2,13 +2,19 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
-// import StarRating from "./StarRating";
+import StarRating from "./StarRating";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <App />
-    {/* <StarRating maxRating={5} color={"#fcc419"} size={20} />
-    <StarRating maxRating={5} color="red" size={30} /> */}
+    <StarRating
+      maxRating={5}
+      color={"#fcc419"}
+      size={20}
+      messages={["Terrible", "Bad", "Okay", "Good", "Amazing"]}
+      defaultRating={3}
+    />
+    <StarRating maxRating={5} color="red" size={30} className="test" />
   </React.StrictMode>
 );
