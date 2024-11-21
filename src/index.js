67741938c947c8c1e77ff3f76649e6175dee4d10
andronicos23 <andronicos.nicolaides@gmail.com>
2024-11-21 11:38:39 +0200

    star outside state passed

--- src/index.js
+++ src/index.js
@@ -1,8 +1,20 @@
 import React from "react";
+import { useState } from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import StarRating from "./StarRating";
+
+function Test() {
+  const [movieRating, setMovieRating] = useState(0);
+
+  return (
+    <div>
+      <StarRating colo="blue" maxRating={10} onSetRating={setMovieRating} />
+      <p>The Rating was set on {movieRating} for this movie.</p>
+    </div>
+  );
+}
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
@@ -16,5 +28,6 @@ root.render(
       defaultRating={3}
     />
     <StarRating maxRating={5} color="red" size={30} className="test" />
+    <Test />
   </React.StrictMode>
 );
